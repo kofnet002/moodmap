a488ce83aa2e2028714d4ae81f5e2ae632f50ce9
kofnet002 <noeljoel61@gmail.com>
2024-08-21 20:15:22 +0000

    add fix for Type 'IterableIterator<number>' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.ts(2802) (method) Array<any>.keys(): IterableIterator<number>

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,63 +1,60 @@
 import { gradients, baseRating, demoData } from "@/util";
 import { FC } from "react";
 
-
 interface PageProps {
-    demo?: boolean
+    demo?: boolean;
 }
 
-const months = { 'January': 'Jan', 'February': 'Feb', 'March': 'Mar', 'April': 'Apr', 'May': 'May', 'June': 'Jun', 'July': 'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct', 'November': 'Nov', 'December': 'Dec' }
-const now = new Date()
-const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = { 'January': 'Jan', 'February': 'Feb', 'March': 'Mar', 'April': 'Apr', 'May': 'May', 'June': 'Jun', 'July': 'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct', 'November': 'Nov', 'December': 'Dec' };
+const now = new Date();
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const Calender: FC<PageProps> = ({ demo }) => {
-    const year = 2024
-    const month = 'August'
-    const monthNow = new Date(year, Object.keys(months).indexOf(month), 1)
-    const firstDayOfMonth = monthNow.getDay()
-    const daysInMonth = new Date(year, Object.keys(month).indexOf(month) + 1, 0).getDate()
+    const year = 2024;
+    const month = 'August';
+    const monthNow = new Date(year, Object.keys(months).indexOf(month), 1);
+    const firstDayOfMonth = monthNow.getDay();
+    const daysInMonth = new Date(year, Object.keys(month).indexOf(month) + 1, 0).getDate();
 
-    const daysToDisplay = firstDayOfMonth + daysInMonth
-    const numRows = (Math.floor(daysToDisplay / 7)) + (daysToDisplay % 7 ? 1 : 0)
-    // console.log('monthNow', monthNow)
-    // console.log('firstDayOfMonth', firstDayOfMonth)
-    // console.log('daysInMonth', daysInMonth)
-    // console.log('daysToDisplay', daysToDisplay)
-    // console.log('numRows', numRows)
+    const daysToDisplay = firstDayOfMonth + daysInMonth;
+    const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0);
+
     return (
         <div className="flex flex-col overflow-hidden gap-1 py-4 sm:py-6 md:py-10">
-            {[...Array(numRows).keys()].map((row: number, rowIndex: number) => {
+            {Array.from(Array(numRows)).map((_, rowIndex: number) => {
                 return (
                     <div key={rowIndex} className="grid grid-cols-7 gap-1">
                         {dayList.map((dayOfWeek: string, dayOfWeekIndex: number) => {
-                            let dayIndex = (row * 7) + dayOfWeekIndex - (firstDayOfMonth - 1)
-                            let dayDisplay = dayIndex > daysInMonth ? false : (row === 0 && dayOfWeekIndex < firstDayOfMonth) ? false : true
-                            let isToday = dayIndex === now.getDate()
+                            let dayIndex = rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
+                            let dayDisplay = dayIndex > daysInMonth ? false : rowIndex === 0 && dayOfWeekIndex < firstDayOfMonth ? false : true;
+                            let isToday = dayIndex === now.getDate();
 
                             if (!dayDisplay) {
-                                return (
-                                    <div className="bg-white" key={dayOfWeekIndex} />
-                                )
+                                return <div className="bg-white" key={dayOfWeekIndex} />;
                             }
 
-                            let color = demo ?
-                                gradients.pink[baseRating[dayIndex]] :
-                                dayIndex in demoData ? gradients.pink[demoData[dayIndex]] : 'white'
+                            let color = demo
+                                ? gradients.pink[baseRating[dayIndex]]
+                                : dayIndex in demoData
+                                    ? gradients.pink[demoData[dayIndex]]
+                                    : 'white';
 
                             return (
-                                <div style={{ background: color }} key={dayOfWeekIndex}
-                                    className={`text-xs sm:text-sm border border-solid p-2 flex items-center gap-2 justify-between rounded-lg ${isToday ? 'border-pink-400' : 'border-pink-100'}
-                                    ${color === 'white' ? 'text-pink-400' : 'text-white'}`}
+                                <div
+                                    style={{ background: color }}
+                                    key={dayOfWeekIndex}
+                                    className={`text-xs sm:text-sm border border-solid p-2 flex items-center gap-2 justify-between rounded-lg ${isToday ? 'border-pink-400' : 'border-pink-100'} ${color === 'white' ? 'text-pink-400' : 'text-white'
+                                        }`}
                                 >
                                     <p>{dayIndex}</p>
                                 </div>
-                            )
+                            );
                         })}
                     </div>
-                )
+                );
             })}
         </div>
-    )
-}
+    );
+};
 
 export default Calender;
